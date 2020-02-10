6f1011efa43859062b4b1d5fbc67d9e2d65e62e2
Bruno Seiji <bruno.seiji.idealtrends@gmail.com>
2020-02-10 16:08:25 -0300

    Lógica todo-list adicionada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+   render() {
+      return(
+         <div>
+            <h1>Lista de Tarefas</h1>
+            <TodoList/>
+         </div>
+      );
+   }
 }
 
 export default App;
